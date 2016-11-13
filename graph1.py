8ee0233067eb1ee8900561bc8f7ee43b78a938f8
AdamRJones <craftadamj@gmail.com>
2016-11-13 00:48:53 -0800

    All three graphs at once

--- graph1.py
+++ graph1.py
@@ -24,55 +24,48 @@ import math as m
 ##t.update()
 
 
-###period and frequency
-##
-### g is acceleration due to gravity = 9.8m/s^2
-##g = 9.8
-##
-##for i in range(1,10):
-##    length = i/10
-##    # compute period
-##    p = 2 * m.pi * m.sqrt(length/g)
-##    # computer frequency
-##    f = 1/p
-##    print (length,p,f)
-##
-##steps = 1000
-##dx = .1
-##x=0
-##amplitude = 100
-##for i in range (0,steps):
-##    x = x + dx
-##    y = amplitude*m.sin(x)
-##    #print (x,y)
-##    graph.setpos (x*10,y)
+# squares
+scale=10
+for i in range(1,10):
+    for j in range(4):
+        graph.forward(i*scale)
+        graph.right(90)
+    graph.penup()
+    graph.forward(-.5*scale)
+    graph.left(90)
+    graph.forward(.5*scale)
+    graph.right(90)
+    graph.pendown()
 
-##scale=10
-##for i in range(1,20):
-##    for j in range(4):
-##        graph.forward(i*scale)
-##        graph.right(90)
-##    graph.penup()
-##    graph.forward(-.5*scale)
-##    graph.left(90)
-##    graph.forward(.5*scale)
-##    graph.right(90)
-##    graph.pendown()
 
-##scale=10
-##for i in range(1,10):
-##    for j in range(3):
-##        graph.forward(i*scale)
-##        graph.left(120)
-##    graph.penup()
-##    graph.forward(-.5*scale)
-##    graph.right(90)
-##    graph.forward(.33*scale)
-##    graph.left(90)
-##    graph.pendown()
+# move pen
+graph.penup()
+graph.setpos(200,0)
+graph.pendown()
 
-scale=40
-graph.setpos(0,0)
+
+# triangles
+scale=10
+for i in range(1,10):
+    for j in range(3):
+        graph.forward(i*scale)
+        graph.left(120)
+    graph.penup()
+    graph.forward(-.5*scale)
+    graph.right(90)
+    graph.forward(.33*scale)
+    graph.left(90)
+    graph.pendown()
+
+
+# move pen
+graph.penup()
+graph.setpos(-200,50)
+graph.pendown()
+
+
+# chessboard
+scale=10
 for i in range(1,10):
     graph.right(90)
     graph.forward(8*scale)
@@ -83,8 +76,9 @@ for i in range(1,10):
     graph.pendown()
 
 graph.penup()
-graph.setpos(0,0)
+graph.setpos(-200,50)
 graph.pendown()
+
 for j in range(1,10):
     graph.forward(8*scale)
     graph.penup()
