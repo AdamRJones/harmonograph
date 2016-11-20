6013144ecb4c623d5168f61cece29d0b9f09d64d
AdamRJones <craftadamj@gmail.com>
2016-11-19 16:36:28 -0800

    Added function, added gimbal, changed parameters

--- harmonograph1.py
+++ harmonograph1.py
@@ -12,11 +12,15 @@
 import turtle as graph
 import math as m
 
+def posn (time,amplitude,frequency,phase,decay):
+    return amplitude* \
+           m.sin(time*frequency+phase)* \
+           m.e**(-decay*time)
 
 ## turtle basics
 ##
 ## make turtle go fast by turning off tracing
-##t.tracer(0,0)
+##graph.tracer(0,0)
 ##
 ##for x in range(500):
 ##    t.forward (x)
@@ -57,24 +61,72 @@ import math as m
 #                   sin(time*frequency + phase_offset) *
 #                   e**(-time*damping_constant)
 
+### x pendulum
+##a1=200
+###f1=3.12
+##p1=0
+##d1=.004
+##
+### y pendulum
+##a2=200
+###f2=3
+##p2=m.pi/2
+##d2=.004
+##
+### gx - gimbal x
+##a3=200
+###f3=3
+##p3=0
+##d3=.004
+##
+### gy- gimbal y
+##a4=200
+###f4=3.13
+##p4=3*m.pi/2
+##d4=.004
+
 # x pendulum
-a1=300
-f1=5
+a1=200
+f1=3.1
 p1=0
-d1=.2
+d1=.04
 
 # y pendulum
-a2=350
-f2=5
-p2=m.pi/4
-d2=.2
+a2=200
+f2=3
+p2=m.pi/3
+d2=.04
+
+# gx - gimbal x
+a3=0
+f3=3
+p3=0
+d3=.004
+
+# gy- gimbal y
+a4=0
+f4=3.1
+p4=3*m.pi/2
+d4=.004
 
 # time and delta
 dt=.01
 t=0
+steps=20000
 
-for i in range(1,2000):
+graph.tracer(0,0)
+
+graph.penup()
+x0=posn(0,a1,f1,p1,d1)+posn(0,a3,f3,p3,d3)
+y0=posn(0,a2,f2,p2,d2)+posn(0,a4,f4,p4,d4)
+graph.setpos(x0,y0)
+graph.pendown()
+
+for i in range(1,steps):
     t=t+dt
-    x=a1*m.sin(t*f1+p1)*m.e**(-d1*t)
-    y=a1*m.sin(t*f2+p2)*m.e**(-d2*t)
+    x=posn(t,a1,f1,p1,d1)+posn(t,a3,f3,p3,d3)
+    y=posn(t,a2,f2,p2,d2)+posn(t,a4,f4,p4,d4)
     graph.setpos(x,y)
+graph.update()
+
+
